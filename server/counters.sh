de013d1c5aa0ebd93ab5fcba4d627f5a9048cc51
Fred Chen <fred.chen@live.com>
2021-01-18 12:59:38 +0800

    change argument position of runtime and interval for counters.sh

--- server/counters.sh
+++ server/counters.sh
@@ -4,8 +4,8 @@
 runtime=36000  # how long, default 10 hours
 interval=60    # how often, default every 60 seconds
 
-[[ ! -z "$1" ]] && interval=$1
-[[ ! -z "$2" ]] && runtime=$2
+[[ ! -z "$1" ]] && runtime=$1
+[[ ! -z "$2" ]] && interval=$2
 
 total=0
 while true
