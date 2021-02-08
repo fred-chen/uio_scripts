f23fedc9ed636c25a4024d6b871c4cd75935c8a7
Fred Chen <fred.chen@live.com>
2021-02-08 19:21:16 +0800

    add support to call init_backend.sh without -S for whole disk.

--- server/init_backend.sh
+++ server/init_backend.sh
@@ -2,11 +2,12 @@
 # wipe all data disks, init DP backend and reserve space for coredumps
 # Maintainer: Fred Chen
 
-CORE_MD_PATH="/dev/md127"
+CORE_MD_PATH="/dev/md/mdcore"
 CORE_MNT="/var/coredumps"
 CORE_SIZE_G=300  # size in GiB reserved for core dump
-DISK_SIZE_G=480  # size in GiB for disks
+DISK_SIZE_G=""   # size in GiB for disks
 OP=
+DISCARD_RUNS=0
 
 function usage { echo "usage: $(basename $0) [ clear|init ] [ -G dumpdev_size ] [ -S disk_size ]" && exit 1; }
 handleopts() {
@@ -30,6 +31,7 @@ function clear() {
   umount ${CORE_MNT} || { mount | grep -w ${CORE_MNT} && { echo "can't umount ${CORE_MNT}"; exit 1; } }
 
   mdadm --stop ${CORE_MD_PATH}
+  mdadm --stop --scan
   while [[ -e ${CORE_MD_PATH} ]]; do { echo "stopping ${CORE_MD_PATH}..."; sleep 1; } done
 
   echo -n "clearing disks ${!devices[@]} ... "
@@ -51,21 +53,24 @@ function reload_partitions() {
 function init() {
   echo -n "init disks ${!devices[@]} ... "
   capacity_k=
-  for d in ${!devices[@]}
-  do
-    sz_b=`blockdev --getsize64 ${d}` && sz_k=`expr ${sz_b} / 1024`
-    capacity_k=$((capacity_k+sz_k))
-  done
 
+  if [[ -z "${DISK_SIZE_G}" ]]; then
+    for d in ${!devices[@]}
+    do
+      sz_b=`blockdev --getsize64 ${d}` && sz_k=`expr ${sz_b} / 1024`
+      capacity_k=$((capacity_k+sz_k))
+    done
+  else
+    capacity_k=`expr ${DISK_SIZE_G} \* ${#devices[@]} \* 1024 \* 1024`
+  fi
   ratio=`echo - | awk "{ print ${CORE_SIZE_G} * 1024 * 1024 / ${capacity_k} }"`
 
-  # discard ssd
+  # wipe ssd
   for d in ${!devices[@]}
   do
     # Discard the content of sectors on a device.
     wipefs -f -a ${d}  > /dev/null 2>&1
     dd if=/dev/zero of=${d} bs=1M count=16 > /dev/null 2>&1
-    reload_partitions
   done
 
   for d in ${!devices[@]}
@@ -75,48 +80,55 @@ function init() {
       sz_b=`blockdev --getsize64 ${d}` && sz_k=`expr ${sz_b} / 1024`
       sz_k_reserved=`echo - | awk "{ print ${sz_k} * ${ratio} }"` && sz_k_reserved=`printf "%.0f" ${sz_k_reserved}`
       offset_G_data=$((($sz_k - $sz_k_reserved) / 1024 / 1024))
-      echo $sz_k, $offset_G_data, $sz_k_reserved
-      parted -s ${d} mklabel gpt
-      parted -s ${d} mkpart primary 0 2%
-      parted -s ${d} mkpart primary 2% ${offset_G_data}GiB
-      parted -s ${d} mkpart primary ${offset_G_data}GiB 100%
+      parted -a optimal -s ${d} mklabel gpt
+      parted -a optimal -s ${d} mkpart primary 1024KiB 2%
+      parted -a optimal -s ${d} mkpart primary 2% ${offset_G_data}GiB
+      parted -a optimal -s ${d} mkpart primary ${offset_G_data}GiB 100%
     else
       sz_k=`expr ${DISK_SIZE_G} \* 1024 \* 1024`
       sz_k_reserved=`echo - | awk "{ print ${sz_k} * ${ratio} }"` && sz_k_reserved=`printf "%.0f" ${sz_k_reserved}`
       offset_G_data=$((($sz_k - $sz_k_reserved) / 1024 / 1024))
       offset_G_Idx=$(($sz_k * 2 / 1024 / 1024 / 100))
-      echo ${DISK_SIZE_G}, $sz_k, $offset_G_data, $sz_k_reserved, $offset_G_Idx
-      parted -s ${d} mklabel gpt
-      parted -s ${d} mkpart primary 0 ${offset_G_Idx}GiB
-      parted -s ${d} mkpart primary ${offset_G_Idx}GiB ${offset_G_data}GiB
-      parted -s ${d} mkpart primary ${offset_G_data}GiB ${DISK_SIZE_G}
+      parted -a optimal -s ${d} mklabel gpt
+      parted -a optimal -s ${d} mkpart primary 1024KiB ${offset_G_Idx}GiB
+      parted -a optimal -s ${d} mkpart primary ${offset_G_Idx}GiB ${offset_G_data}GiB
+      parted -a optimal -s ${d} mkpart primary ${offset_G_data}GiB ${DISK_SIZE_G}GiB
     fi
-    reload_partitions
   done
 
   core_devs=
-  make a mdadm raid for coredump dev
+  # make a mdadm raid for coredump dev
   for d in ${!devices[@]}
   do
     core_devs="$core_devs ${d}3"
+    wipefs -f -a ${d}1  > /dev/null 2>&1
+    wipefs -f -a ${d}2  > /dev/null 2>&1
     wipefs -f -a ${d}3  > /dev/null 2>&1
   done
+  mdadm --stop --scan
+  while [[ -e ${CORE_MD_PATH} ]]; do { mdadm --stop ${CORE_MD_PATH}; sleep 1; } done
   [ ! -z "$core_devs" ] && {
     yes | mdadm --create ${CORE_MD_PATH} --level=raid0 --raid-devices=${#devices[@]} ${core_devs};
-  } || { echo "failed to create core dump raid device" && exit 1; }
+  } || { echo "failed to create core dump raid device" && return 1; }
 
   while [[ ! -e ${CORE_MD_PATH} ]]; do { sleep 1; } done
 
   # discard index and data partitions
-  for d in ${!devices[@]}
-  do
-    blkdiscard ${d}1 &
-    blkdiscard ${d}2 &
-  done && wait
+  [[ ${DISCARD_RUNS} -eq 0 ]] && {
+    for d in ${!devices[@]}
+    do
+      blkdiscard ${d}1 &
+      blkdiscard ${d}2 &
+    done && wait
+    DISCARD_RUNS=$((DISCARD_RUNS+1))
+  }
 
   mkdir -p ${CORE_MNT}
   # mount core dump device
   mkfs -t ext4 ${CORE_MD_PATH} && mount ${CORE_MD_PATH} ${CORE_MNT}
+  mounted=`df -h | grep /var/coredumps | wc -l`
+  [[ $mounted -eq 0 ]] && retuen 1
+
   sysctl -w kernel.core_pattern=/var/coredumps/core-%e-sig%s-user%u-group%g-pid%p-time%t
   ulimit -c unlimited
 
@@ -146,7 +158,17 @@ function main() {
   echo "backend devices: ${!devices[@]}"
   [[ ! -z "$OP" ]] && [[ ! "$OP" =~ clear|init ]] && usage
   [[ "$OP" == "clear" ]] && clear && exit 0
-  [[ "$OP" == "init"  ]] && { clear && init; } && exit 0
+  if [[ "$OP" == "init"  ]]; then
+    i=0
+    while [[ $i -le 3 ]]; do
+      clear
+      if init; then
+        exit 0
+      fi
+      i=$((i+1))
+      echo "faied. retry ... $i"
+    done
+  fi
 }
 
 main $@
